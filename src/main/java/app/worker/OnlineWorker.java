709b01431569d0884725d13b0c5402caeed2413d
ADAM-PC\adamz <adamzimnyy@gmail.com>
2018-01-30 15:26:28 +0100

    log saved online

--- src/main/java/app/worker/OnlineWorker.java
+++ src/main/java/app/worker/OnlineWorker.java
@@ -1,15 +1,11 @@
 package app.worker;
 
 import app.model.Online;
-import app.repository.CharacterRepository;
 import app.repository.OnlineRepository;
 import app.util.OnlineParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.ZonedDateTime;
-import java.util.Date;
 
 /**
  * Created by adamz on 30.01.2018.
@@ -20,9 +16,10 @@ public class OnlineWorker {
     @Autowired
     OnlineRepository onlineRepository;
 
-    @Scheduled(cron ="0 0/30 0 ? * *")
+    @Scheduled(cron = "0 0/30 0 ? * *")
     public void checkOnline() {
         Online on = OnlineParser.parse();
+        System.out.println("Online saved: Feronis = " + on.getFeronis() + ", Angrathar = " + on.getAngrathar());
         onlineRepository.save(on);
     }
 }
