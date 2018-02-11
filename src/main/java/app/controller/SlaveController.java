e65b5e5bad33298fa3681fb63f8264a3024d0a2e
ADAM-PC\adamz <adamzimnyy@gmail.com>
2018-02-11 16:53:03 +0100

    slave market backend

--- src/main/java/app/controller/SlaveController.java
+++ src/main/java/app/controller/SlaveController.java
@@ -1,21 +1,25 @@
 package app.controller;
 
+import app.model.Slave;
+import app.repository.SlaveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 
 @RestController
 public class SlaveController {
 
+    @Autowired
+    SlaveRepository slaveRepository;
+
     @CrossOrigin
     @RequestMapping(value = {"/slave", "slave"}, method = RequestMethod.POST)
     public @ResponseBody
-    void getSlaves(@RequestBody Map<String, Integer> slaves) {
-        System.out.println("Received slaves: ");
-        for (String s : slaves.keySet()                ) {
-            System.out.println(s+" = "+slaves.get(s));
-        }
+    List<Slave> getSlaves() {
+       return slaveRepository.findAll();
     }
 
 }
